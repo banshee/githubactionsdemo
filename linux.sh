71ef65372af4b7feef38ed10ea34388f44e9ff66
James Moore <james@restphone.com>
2020-11-25 11:17:21 -0800

    work in progress

--- linux.sh
+++ linux.sh
@@ -1,3 +1,5 @@
+set -e
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
