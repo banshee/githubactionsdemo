57ce3a10a8da9d64fe6e45b26300d75fa84d3ff1
James Moore <james@restphone.com>
2020-11-25 12:54:24 -0800

    work in progress

--- linux.sh
+++ linux.sh
@@ -3,6 +3,11 @@ set -ex
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+
 # npm install of these succeeds, but then returns 1 as its exit value.  Just
 # assume it worked; if it didn't, everything will die immediately
 sudo npm install -g truffle @truffle/hdwallet-provider ganache-cli || true
