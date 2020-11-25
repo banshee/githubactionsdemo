074d7cfa1c7d3ed0922ddfd60b8b6629595d4038
James Moore <james@restphone.com>
2020-11-25 11:54:22 -0800

    remove nvm custom

--- linux.sh
+++ linux.sh
@@ -1,9 +1,5 @@
 set -e
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
 nvm install 14.15.1
 
 # npm install of these succeeds, but then returns 1 as its exit value.  Just
