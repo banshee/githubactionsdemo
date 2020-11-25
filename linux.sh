50bc4b0990f7e5403681ffb063c29ce87e94685f
James Moore <james@restphone.com>
2020-11-25 12:04:29 -0800

    work in progress

--- linux.sh
+++ linux.sh
@@ -1,12 +1,11 @@
-set -e
+set -ex
 
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # npm install of these succeeds, but then returns 1 as its exit value.  Just
 # assume it worked; if it didn't, everything will die immediately
-npm install -g truffle @truffle/hdwallet-provider ganache-cli || true
+sudo npm install -g truffle @truffle/hdwallet-provider ganache-cli || true
 
 # these npm packages were written correctly
-npm install -g dotenv
-
+sudo npm install -g dotenv
