90737bbfc23bf79a677715bbce2a3a1277f8bd10
James Moore <james@restphone.com>
2020-11-25 12:01:24 -0800

    work in progress

--- linux.sh
+++ linux.sh
@@ -1,10 +1,7 @@
 set -e
 
-export NVM_DIR=~/.nvm
-source ~/.nvm/nvm.sh
-cat ~/.nvm/nvm.sh
-
-nvm install 14.15.1
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 # npm install of these succeeds, but then returns 1 as its exit value.  Just
 # assume it worked; if it didn't, everything will die immediately
