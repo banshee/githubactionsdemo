1f15be91a3962a9da405d2e4e4c5d3fd66dd30c2
James Moore <james@restphone.com>
2020-11-25 14:32:20 -0800

    work in progress

---
+++ addenvtobashrc.sh
@@ -0,0 +1,6 @@
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export PATH=$GOROOT/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=$GOBIN:$PATH' >> ~/.bashrc
+echo 'export PATH=$HOME/.npm-global/bin:$PATH' >> ~/.bashrc
--- linux.sh
+++ linux.sh
@@ -13,3 +13,10 @@ npm install -g truffle @truffle/hdwallet-provider ganache-cli || true
 
 # these npm packages were written correctly
 npm install -g dotenv
+
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export PATH=$GOROOT/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=$GOBIN:$PATH' >> ~/.bashrc
+echo 'export PATH=$HOME/.npm-global/bin:$PATH' >> ~/.bashrc
