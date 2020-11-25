bf3d89708c6010b8cedfb5e859851bc9ce80ef5c
James Moore <james@restphone.com>
2020-11-25 14:38:12 -0800

    work in progress

--- addenvtobashrc.sh
+++ addenvtobashrc.sh
@@ -1,7 +1,7 @@
-echo 'export AAA=$HOME/go' >> ~/.bashrc
-echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
-echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
-echo 'export PATH=$GOROOT/bin:$PATH' >> ~/.bashrc
-echo 'export PATH=$GOBIN:$PATH' >> ~/.bashrc
-echo 'export PATH=$HOME/.npm-global/bin:$PATH' >> ~/.bashrc
+echo 'export AAA=$HOME/go' >> ~/.bash_profile
+echo 'export GOPATH=$HOME/go' >> ~/.bash_profile
+echo 'export GOBIN=$GOPATH/bin' >> ~/.bash_profile
+echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+echo 'export PATH=$GOROOT/bin:$PATH' >> ~/.bash_profile
+echo 'export PATH=$GOBIN:$PATH' >> ~/.bash_profile
+echo 'export PATH=$HOME/.npm-global/bin:$PATH' >> ~/.bash_profile
